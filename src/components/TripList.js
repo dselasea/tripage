0b26609aeaec97ef57da52f5d77e19a13fe18131
Daniel <dselasea@gmail.com>
2022-09-15 20:24:26 +0000

    Added styles

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,18 +1,30 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import "./TripList.css"
 
 
 const TripList = () => {
-  // const [trips, setTrips] = useState([])
-  // console.log(trips)
+  const [trips, setTrips] = useState([])
+  console.log(trips)
 
-  //  fetch('http://localhost:3000/trips')
-  //     .then(response => response.json())
-  //     .then(json => setTrips(json))
+  useEffect(() => {
+    fetch('http://localhost:3000/trips')
+       .then(response => response.json())
+       .then(json => setTrips(json))
+  }, [])
+
 
   return (
-    <div>
+    <div className='trip-list'>
         <h2>Trip List</h2>
+        <ul>
+          {trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
