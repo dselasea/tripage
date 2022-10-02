ad3861b85a9e91406a09c1652807ec56c609e9a7
Daniel <dselasea@gmail.com>
2022-10-02 02:06:08 +0000

    useEffect, added depenency array

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,38 +1,36 @@
 import React from 'react'
+// Styles
+import './TripList.css'
 import { useState, useEffect } from 'react'
-import "./TripList.css"
-
 
 const TripList = () => {
-  const [trips, setTrips] = useState([])
-  const [url, setUrl] = useState('http://localhost:3000/trips')
-  console.log(trips)
+    const [trips, setTrips] = useState([])
+    const [url, setUrl] = useState('http://localhost:3000/trips')
 
-  useEffect(() => {
-    fetch(url)
-       .then(response => response.json())
-       .then(json => setTrips(json))
-  }, [url])
-
+    useEffect(() => {
+        fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(setTrips(json)))
+    },[url])
+    
+    console.log(trips)
 
   return (
     <div className='trip-list'>
         <h2>Trip List</h2>
         <ul>
-          {trips.map((trip) => (
-            <li key={trip.id}>
-              <h3>{trip.title}</h3>
-              <p>{trip.price}</p>
-            </li>
-          ))}
+            {trips.map(trip => (
+                <li key={trip.id}>
+                    <h3>{trip.title}</h3>
+                    <p>{trip.price}</p>
+                </li>
+            ))}
         </ul>
         <div className="filters">
-          <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
-            European Trips
-          </button>
-          <button onClick={() => setUrl('http://localhost:3000/trips')}>
-            All Trips
-          </button>
+            <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+                Europen Trips
+            </button>
+            <button onClick={() => setUrl('http://localhost:3000/trips')}>All Trips</button>
         </div>
     </div>
   )
